85e6b4dd6e62186f86a87789840fc01efba46ad6
Pranjil97 <pranjilkumar97@gmail.com>
2023-04-27 01:39:06 +0530

    update typewriter speed

--- src/Components/Typewrite.jsx
+++ src/Components/Typewrite.jsx
@@ -5,20 +5,32 @@ const Typewrite = () => {
     return (
         <div className='typewrite'>
             <Typewriter
-                onInit={(typewriter) => {
-                    typewriter
-                        .typeString("Welcome To Innovational Accounting, Smart Accounting For Smart People")
-                        .pauseFor(100)
-                        .deleteAll()
-                        .typeString("Welcome To Innovational Accounting, Smart Accounting For Smart People")
-                        .start();
+
+options={{
+    strings: ["Welcome To Innovational Accounting, Smart Accounting For Smart People"],
+    autoStart: true,
+    loop: false,
+    delay: 50,
+    pauseFor: 10000,
+    typingSpeed: 25,
+    deleteSpeed: 50,
+    cursor: "_",
+  }}
+
+                // onInit={(typewriter) => {
+                //     typewriter
+                //         .typeString("Welcome To Innovational Accounting, Smart Accounting For Smart People")
+                //         .pauseFor(80)
+                //         .deleteAll()
+                //         .typeString("Welcome To Innovational Accounting, Smart Accounting For Smart People")
+                //         .start();
 
                     // .typeString("Welcome To GATP Solutions")
                     // .pauseFor(100)
                     // .deleteAll()
                     // .typeString("Smart Accounting For Smart People")
                     // .start();
-                }}
+                // }}
             />
         </div>
     )
